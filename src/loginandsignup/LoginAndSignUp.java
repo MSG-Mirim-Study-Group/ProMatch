85f4e8252c8936daa3725c2e79b45b9b704d5cdb
hyewon4052 <s2311@e-mirim.hs.kr>
2023-10-16 05:31:10 +0900

    로그인 및 회원가입

--- src/loginandsignup/LoginAndSignUp.java
+++ src/loginandsignup/LoginAndSignUp.java
@@ -2,7 +2,7 @@ package loginandsignup;
 
 public class LoginAndSignUp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {    // 맨 처음 로그인 창으로 나오게 함
         Login LoginFrame = new Login();
         LoginFrame.setVisible(true);
         LoginFrame.pack();
