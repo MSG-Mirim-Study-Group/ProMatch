75bdd77b9166c4db2375b07f6b0950e91a5ea9b9
hyewon4052 <s2311@e-mirim.hs.kr>
2023-10-16 05:30:03 +0900

    로그인

--- src/loginandsignup/Login.java
+++ src/loginandsignup/Login.java
@@ -4,7 +4,7 @@ public class Login extends javax.swing.JFrame {
 
     public Login() {
         initComponents();
-    }
+    }   //텍스트 상자, 레이블, 버튼 등을 초기화하고 배치하기 위해 initComponents 메서드 사용
 
     @SuppressWarnings("unchecked")
     private void initComponents() {
@@ -30,44 +30,48 @@ public class Login extends javax.swing.JFrame {
         jPanel2.setPreferredSize(new java.awt.Dimension(800, 500));
         jPanel2.setLayout(null);
 
+        //사실 왼쪽인데 RIGHT이라고 잘못씀..ㅠㅠ 왼쪽에 있는 패널 색, 크기 지정
         Right.setBackground(new java.awt.Color(80, 175, 217));
         Right.setMinimumSize(new java.awt.Dimension(400, 500));
         Right.setPreferredSize(new java.awt.Dimension(800, 500));
 
+        //프로매치 아이콘 삽입
         jLabel5.setIcon(new javax.swing.ImageIcon("C:\\Users\\hyewo\\OneDrive\\Documents\\NetBeansProjects\\LoginAndSignUp\\src\\Icon\\logo (3).png")); // NOI18N
-        jLabel5.setText("jLabel5");
+        jLabel5.setText("jLabel5"); // Jlabel5 = 프로매치 아이콘 이미지역할
 
         javax.swing.GroupLayout RightLayout = new javax.swing.GroupLayout(Right);
         Right.setLayout(RightLayout);
         RightLayout.setHorizontalGroup(
-                RightLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                RightLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)  // 컴포넌트 왼쪽 정렬
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, RightLayout.createSequentialGroup()
-                                .addComponent(jLabel5)
-                                .addGap(0, 0, Short.MAX_VALUE))
+                                .addComponent(jLabel5)  //jLabel5 컴포넌트를 RightLayout에 추가
+                                .addGap(0, 0, Short.MAX_VALUE)) //공백을 없애고 가장 오른쪽으로 정렬
         );
+
         RightLayout.setVerticalGroup(
                 RightLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(RightLayout.createSequentialGroup()
-                                .addGap(200, 200, 200)
+                                .addGap(200, 200, 200)  //jLabel5를 위에서 200 픽셀, 아래에서 200 픽셀로 내림 -> jLabel5가 패널의 중앙에 위치
                                 .addComponent(jLabel5)
-                                .addContainerGap(206, Short.MAX_VALUE))
+                                .addContainerGap(206, Short.MAX_VALUE)) // jLabel5 아래 여백을 없애고 컴포넌트를 패널의 아래쪽에 정렬
         );
 
+        // 이메일, 패스워드 레이블의 폰트 및 크기 설정
         jPanel2.add(Right);
         Right.setBounds(0, 0, 400, 500);
 
         Left.setBackground(new java.awt.Color(255, 255, 255));
         Left.setMinimumSize(new java.awt.Dimension(400, 500));
 
-        jLabel1.setFont(new java.awt.Font("맑은 고딕", 1, 36)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("맑은 고딕", 1, 36));
         jLabel1.setForeground(new java.awt.Color(80, 175, 217));
         jLabel1.setText("LOGIN");
 
-        jLabel2.setFont(new java.awt.Font("맑은 고딕", 1, 14)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("맑은 고딕", 1, 14));
         jLabel2.setForeground(new java.awt.Color(102, 102, 102));
         jLabel2.setText("Email");
 
-        jTextField1.setFont(new java.awt.Font("맑은 고딕", 0, 14)); // NOI18N
+        jTextField1.setFont(new java.awt.Font("맑은 고딕", 0, 14));
         jTextField1.setForeground(new java.awt.Color(102, 102, 102));
         jTextField1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -75,7 +79,7 @@ public class Login extends javax.swing.JFrame {
             }
         });
 
-        jLabel3.setFont(new java.awt.Font("맑은 고딕", 1, 14)); // NOI18N
+        jLabel3.setFont(new java.awt.Font("맑은 고딕", 1, 14));
         jLabel3.setForeground(new java.awt.Color(102, 102, 102));
         jLabel3.setText("Password");
 
@@ -108,9 +112,11 @@ public class Login extends javax.swing.JFrame {
             }
         });
 
+        // leftlayout 객체 생성후 left 패널에 적용
+
         javax.swing.GroupLayout LeftLayout = new javax.swing.GroupLayout(Left);
         Left.setLayout(LeftLayout);
-        LeftLayout.setHorizontalGroup(
+        LeftLayout.setHorizontalGroup(  //수평으로 요소를 배치하기 위한 그룹을 설정하기 위해 setHorizontalGroup 메서드를 사용
                 LeftLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(LeftLayout.createSequentialGroup()
                                 .addGroup(LeftLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -122,6 +128,7 @@ public class Login extends javax.swing.JFrame {
                                                                 .addComponent(jTextField1)
                                                                 .addComponent(jLabel3)
                                                                 .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, 348, javax.swing.GroupLayout.PREFERRED_SIZE))
+
                                                         .addGroup(LeftLayout.createSequentialGroup()
                                                                 .addComponent(jLabel4)
                                                                 .addGap(45, 45, 45)
@@ -132,14 +139,16 @@ public class Login extends javax.swing.JFrame {
                                                 .addComponent(jLabel1)))
                                 .addContainerGap(28, Short.MAX_VALUE))
         );
-        LeftLayout.setVerticalGroup(
+
+        LeftLayout.setVerticalGroup(    // 수직으로 요소 배치를 위한 그룹 설정
                 LeftLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(LeftLayout.createSequentialGroup()
-                                .addGap(42, 42, 42)
-                                .addComponent(jLabel1)
+                                .addGap(42, 42, 42) // 공백 추가
+                                .addComponent(jLabel1)  //로그인 텍스트를 42픽셀 아래 추가
                                 .addGap(46, 46, 46)
                                 .addComponent(jLabel2)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                //이메일 입력 가능한 텍스트필드 설정
                                 .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(18, 18, 18)
                                 .addComponent(jLabel3)
@@ -190,14 +199,15 @@ public class Login extends javax.swing.JFrame {
 
     }
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {  //login 버튼을 클릭했을 때 호출
         SignUp SignUpFrame = new SignUp();
         SignUpFrame.setVisible(true);
         SignUpFrame.pack();
         SignUpFrame.setLocationRelativeTo(null);
-        this.dispose();
+        this.dispose(); //현재 로그인 창 닫음
     }
 
+    // 지금까지 쓴 gui 요소들 선언
     private javax.swing.JPanel Left;
     private javax.swing.JPanel Right;
     private javax.swing.JButton jButton1;
