a529721385f2d3e7a173e891ce8572efd7c3a072
hyewon4052 <hyewon4052@gmail.com>
2023-10-17 17:20:12 +0900

    loginandsignup

--- src/loginandsignup/LoginAndSignUp.java
+++ src/loginandsignup/LoginAndSignUp.java
@@ -1,12 +1,10 @@
 package loginandsignup;
 
 public class LoginAndSignUp {
-
     public static void main(String[] args) {    // 맨 처음 로그인 창으로 나오게 함
         Login LoginFrame = new Login();
         LoginFrame.setVisible(true);
         LoginFrame.pack();
         LoginFrame.setLocationRelativeTo(null);
     }
-
 }
