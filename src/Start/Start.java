772cb555a5102048ca159afdc713a8240b2480ea
hyewon4052 <s2311@e-mirim.hs.kr>
2023-10-23 18:21:27 +0900

    Start

--- src/Start/Start.java
+++ src/Start/Start.java
@@ -1,6 +1,6 @@
 package Start;
 
-public class LoginAndSignUp {
+public class Start {
     public static void main(String[] args) {    // 맨 처음 로그인 창으로 나오게 함
         Login LoginFrame = new Login();
         LoginFrame.setVisible(true);
